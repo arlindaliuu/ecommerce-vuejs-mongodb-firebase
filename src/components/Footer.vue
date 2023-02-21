60fdc60ec80679b650150471181ba8a48502007e
Zejdi1 <zl51041@utb-uni.net>
2023-02-22 00:05:12 +0100

    footer added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,49 +1,67 @@
 <template>
-    <v-footer
-     
-      padless
-    >
-      <v-card
-        flat
-        tile
-        class="indigo lighten-1 white--text text-center"
-      >
-        <v-card-text>
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-4 white--text"
-            icon
-          >
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
-  
-        <v-card-text class="white--text pt-0">
-          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        </v-card-text>
-  
-        <v-divider></v-divider>
-  
-        <v-card-text class="white--text">
-          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
-  </template>
-  
-<script>
-  export default {
-    name: "Footer",
-    data: () => ({
-      icons: [
-        'mdi-facebook',
-        'mdi-twitter',
-        'mdi-linkedin',
-        'mdi-instagram',
-      ],
-    }),
-  }
-</script>
+  <body class="bg-gray-200">
+      <footer>
+          <div class="p-10 bg-gray-800 text-gray-200 ">
+              <div class="max-w-7xl mx-auto">
+                  <div class="grid grid-cols-1 md-cols-2 lg:grid-cols-4 gap-2">
+                      <div class="mb-5">
+                          <h4 class="text-2xl pb-4">Company</h4>
+                        <p class="text-gray-500">
+                          Selami Hallaci <br>
+                          Presheve <br><br>
+                          <strong>Phone</strong> +381638124036<br>
+                          <strong>Email</strong> a.aliu@gmail.com<br>
+                        </p>
+                      </div>
+                      <div class="mb-5">
+                          <h4>Useful Links</h4>
+                          <ul class="text-gray-500">
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Home</a></li>         
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">About</a></li>
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Services</a></li>
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Terms of Services</a></li>
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Privacy policy</a></li>
+                          </ul>
+                      </div> 
+                      <div class="mb-5">
+                          <h4 class="pb-4">Our Services</h4>
+                          <ul class="text-gray-500">
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Web design</a></li>
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Web Development</a></li>
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">roduct Menagement</a>P</li>
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Marketing</a></li>
+                              <li class="pb-4"><i class="fa fa-chevron-right text-yellow-500"></i><a href="#" class="hover:text-yellow-500">Graphic design</a></li>
+                          </ul>
+                      </div>
+                      <div class="mb-5">
+                          <h4 class="pb-4">Join Our Newsletter</h4>
+                          <p class="text-gray-500 pb-2">Join 25.000 others and neves miss out on new tips tutorials and more</p>
+                          <form class="flex flex-row flex-wrap">
+                             <label> <input type="text" class="text-gray-500 w-2/3 p-2 focus:border-yellow-500" placeholder="email@gmail.com"> </label>
+                              <button class="p-2 w-1/3 bg-yellow-500 text-white hover:bg-yellow-600">Subscribe</button>
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="w-full bg-gray-900 text-gray-500 pc-10">
+              <div class="max-w-7xl flex flex-col sm:flex-row py-4 mx-auto justify-between items-center">
+                  <div class="text-center">
+                      Copyright <strong><span>Company</span></strong> All Right Reserve
+                  </div>
+                  <div>
+                      Designed by <a href="#" class="text-yellow-500">TailwindCss</a>
+                  </div>
+              </div>
+              <div class="text-center text-xl text-white mb-2">
+              <a href="#" class="w-10 h-10 rounded-full bg-yellow-500 hover:bg-yellow-600 mx-1 inline-block pt-1"><i class="fa-brands fa-instagram"></i></a>
+              <a href="#" class="w-10 h-10 rounded-full bg-yellow-500 hover:bg-yellow-600 mx-1 inline-block pt-1"><i class="fa-brands fa-twitter"></i></a>
+              <a href="#" class="w-10 h-10 rounded-full bg-yellow-500 hover:bg-yellow-600 mx-1 inline-block pt-1"><i class="fa-brands fa-facebook"></i></a>
+              <a href="#" class="w-10 h-10 rounded-full bg-yellow-500 hover:bg-yellow-600 mx-1 inline-block pt-1"><i class="fa-brands fa-skype"></i></a>
+              <a href="#" class="w-10 h-10 rounded-full bg-yellow-500 hover:bg-yellow-600 mx-1 inline-block pt-1"><i class="fa-brands fa-linkedin"></i></a>
+          </div>
+          </div> 
+      </footer>
+
+  </body>
+</template>

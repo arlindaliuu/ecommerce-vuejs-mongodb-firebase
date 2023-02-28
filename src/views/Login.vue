75f0b1d3681ea489340bb93e84f6f51187b0980c
Zejdi1 <zl51041@utb-uni.net>
2023-02-28 19:36:46 +0100

    footer

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,52 +1,185 @@
 <template>
-    <main>
-        <section class="forms">
-            <form @submit.prevent="register" class="register">
-                <h2>Register</h2>
-                <input type="email" placeholder="Email adress" v-model="register_form.email" />
-                <input type="password" v-model="register_form.password" />
-                <input type="submit" value="Register">
-            </form>
-            <form @submit.prevent="login" class="login">
-                <h2>Login</h2>
-                <input type="email" placeholder="Email adress" v-model="login_form.email" />
-                <input type="password" v-model="login_form.password" />
-                <input type="submit" value="Login">
-            </form>
-        </section>
-    </main>
+    <article>
+        <div class="container" :class="{'sign-up-active' : signUp}">
+            <div class="overlay-container">
+                <div class="overlay">
+                    <div class="overley-left">
+                        <h1>Miresevini</h1>
+                        <p>Ju lutemi identifikohuni me të dhënat tuaja personale</p>
+                        <button class="invert" id="signIn" @click="signUp = !signUp">Sign In</button>
+                    </div>
+                    <div class="overlay-right">
+                    <h1>Pershendetje</h1>
+                        <p>Ju lutemi shkruani të dhënat tuaja personale</p> 
+                        <button class="invert" id="signUp" @click="signUp = !signUp">Sign In</button>
+                </div>
+            </div>
+        </div>  
+        <form class="sign-up" action="#">
+            <h2>Krijo login </h2>
+            <div>Përdorni emailin tuaj për regjistrim</div>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Sign Up</button>
+        </form>
+        <form class="sign-in" action="#">
+            <h2>Sign In </h2>
+            <div> Përdorni llogarinë tuaj</div>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <a href="#">Keni harruar passwordin</a>
+            <button>Sign Up</button>
+        </form>
+    </div>
+    </article>
 </template>
+
 <script>
-import {ref} from 'vue'
-import { useStore } from 'vuex'
 export default{
-    setup(){
-        const login_form = ref({});
-        const register_form = ref({});
-        const store = useStore();
-
-        const login = () =>{
-            store.dispatch('login', login_form.value);
-        }
-        const register = () =>{
-            store.dispatch('register', register_form.value);
-        }
-
+    data:() => {
         return{
-            login_form,
-            register_form,
-            login,
-            register
+           
         }
     }
+
 }
 </script>
 <style scoped>
-body{
-    background-color: rebeccapurple;
+.container {
+    position:relative;
+    width: 768px;
+    height: 480px;
+    border-radius: 10px;
+    overflow: hidden;
+    background: linear-gradient(to bottom, #efefef, #ccc);
+     box-shadow: 0 15px 30px rgba(0, 0, 0, .2),
+                0 15px 30px rgba(0, 0, 0, .2),           
+}
+.overlay-container{
+    position: absolute;
+    top: 0;
+    left: 50%;
+    width: 50%;
+    height: 100%;
+    overflow: hidden;
+    transition: transorm .5s ease-in-out;
+    z-index: 100;
+}
+.overlay {
+    position: relative;
+    left: -100%;
+    height: 100%;
+    width: 200%;
+    background: linear-gradient(to bottom,#7FD825, #009345 );
+    color: #fff;
+    transform: translateX(0);
+    transition: transform .5s ease-in-out;
+}
+@mixin overlays($property) {
+    position: absolute;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    padding: 70px 40px;
+    width: calc (50% - 80px);
+    height: calc (100% - 140px);
+    text-align: center;
+    transform: translateX($property);
+    transition: transform .5s ease-in-out;
+}
+.overlay-left{
+    @include overlays(-20%);
+
+}
+.overlay-right { 
+    @include overlays(0);
+    right: 0;
+}
+
+h2{
+    margin: 0;
+}
+p{
+    margin: 20px 0 30px;
+}
+a{
+    color: #222;
+    text-decoration: none;
+    margin: 15px 0;
+    font-size: 1rem;
+
+}
+button {
+    border-radius: 20px;
+    border: 1px solid #009345;
+    background-color: #009345;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: bold;
+    padding: 10px 40px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    transition: transform .1s ease-in;
+
+ 
+}
+button.invert{
+    background-color: transparent;
+    border-color: #fff;
+}
+
+form{
+    position: absolute;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column;
+    padding: 90px 60px;
+    width: calc(50% - 120px);
+    height: calc(100% - 180px);
+    text-align: center;
+    background: linear-gradient(to bottom, #efefef,#ccc);
+    transition: all .5s ease-in-out;
     
 }
-h2{
-    color:red;
+input {
+        background-color: #eee;
+        border: none;
+        padding: 8px 15px;
+        margin: 6px 0;
+        width: calc(100% - 30px);
+        border-radius: 15px;
+        border-bottom: 1px solid #ddd;
+        box-shadow: inset 0 1px 2px rgba(0, 0, 0, .4),
+                          0 -1px 1px #fff,
+                          0 1px 0 #fff;
+        overflow: hidden;
+    }
+  
+.sign-in{
+    left: 0;
+    z-index: 2;
 }
+.sign-up{
+    left: 0;
+    z-index: 1;
+    opacity: 0;
+}
+.sign-up {
+  transform: translateX(100%);
+  opacity: 1;
+  z-index: 5;
+    
+}
+.overlay-container {
+    transform: translateX(-100%);
+    }
+    .overlay {
+        transform: translateX(50%);
+    }
+
 </style>
